6037612d13b15e9d931c6ff0468fc2cc591bc12a
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2024-03-19 06:55:32 +0530

    updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import {Routes,Route} from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import About from './pages/About';
-import Policy from './pages/Policy';
 import Notfound from './pages/Notfound';
 //import { ToastContainer } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 import  AdminMovieDashboard  from './pages/MovieManagement/AdminMovieDashboard';
 import AddMovies from './pages/MovieManagement/AddMovies';
 import  CreateMovieCategory  from './pages/MovieManagement/CreateMovieCategory';
@@ -24,7 +23,6 @@ function App() {
       <Route path='/moviedetails/:slug' element={<MovieDetails />} />
       <Route path='/movies' element={<Movies />} />
       <Route path="/about" element={<About />} />
-      <Route path="/policy" element={<Policy />} />
       <Route path="*" element={<Notfound />} />
       <Route path="/adminmoviedashboard" element={<AdminMovieDashboard />} />
       <Route path="/adminmoviedashboard/moviemanagement/add-movie" element={<AddMovies />} />

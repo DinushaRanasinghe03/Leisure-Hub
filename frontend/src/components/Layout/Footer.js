8ff669f2d8ab8601d8d0abafdb9173d025470baa
Dinusha Ranasinghe <dinushaumayangani02@gmail.com>
2024-04-02 22:01:50 +0530

    Header footer changes commited

--- frontend/src/components/Layout/Footer.js
+++ frontend/src/components/Layout/Footer.js
@@ -5,8 +5,13 @@ const Footer = () => {
     <div className="footer">
       <h1 className="text-center"> All Right Reserved &copy;Leisurehub</h1>
       <p className="text-center mt-3">
-        <Link to="/about">About</Link>|<Link to="/contact">Contact</Link>|
-        <Link to="/policy">Privacy Policy</Link>
+        <Link to="/about">About Us</Link>|<Link to="/contact">Contact Us</Link>|
+        <Link to="/policy">Privacy & Policy</Link>|
+        <Link to="/reviews">Reviews & Ratings</Link>|
+        <Link to="/feedback">Provide Feedback To LEISUREHUB</Link>
+      </p>
+      <p className="text-center">
+        0786453567 | A810,Kaduwela | info@leisurehub.gov.lk
       </p>
     </div>
   );
--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-import SearchInput from "../Form/SearchInput";
+//import LEISUREHUB_LOGO from '../assets/LEISUREHUB_LOGO.jpg'
+import LEISUREHUB_LOGO from "../../assets/LEISUREHUB_LOGO.jpg";
 
 const Header = () => {
   return (
@@ -18,28 +19,68 @@ const Header = () => {
           >
             <span className="navbar-toggler-icon" />
           </button>
+
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <Link to="/" className="navbar-brand">
-              Leisurehub App
+            <Link
+              to="/"
+              className="navbar-brand"
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              <img
+                src={LEISUREHUB_LOGO}
+                alt="logo"
+                style={{ maxHeight: "50px", marginRight: "10px" }}
+              />
+              <span style={{ marginTop: "5px" }}>LEISUREHUB</span>
             </Link>
+
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink to="/" className="nav-link ">
+                <NavLink to="/" className="nav-link">
                   Home
                 </NavLink>
               </li>
+
               <li className="nav-item">
-                <NavLink to="/category" className="nav-link">
-                  Category
+                <NavLink to="/movies" className="nav-link">
+                  Movies
                 </NavLink>
               </li>
+
               <li className="nav-item">
-                <NavLink to="/games and activities" className="nav-link">
+                <NavLink to="/schedules" className="nav-link">
+                  Movie Show Times
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink to="/booking" className="nav-link">
+                  Booking
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink to="/gamesandactivities" className="nav-link">
                   Games & Activities
                 </NavLink>
               </li>
+
               <li className="nav-item">
-                <NavLink to="/*" className="nav-link"></NavLink>
+                <NavLink to="/facilities" className="nav-link">
+                  Facilities
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink to="/membership" className="nav-link">
+                  Memberships
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink to="/login" className="nav-link">
+                  Login
+                </NavLink>
               </li>
             </ul>
           </div>

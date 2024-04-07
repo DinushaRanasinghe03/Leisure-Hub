e849f098e2b97d414f5eba3eef505cfab12b36f8
edwithanage <erandiwithanage20@gmail.com>
2024-04-07 13:54:19 +0530

    update css

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -1,21 +1,43 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "../Header/index.css";
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import "../Header/index.css";
+// const Footer = () => {
+//   return (
+//     <div className="footer">
+//       <h4 className="text-center">All Right Reserved &copy; LEISUREHUB</h4>
+//       <p className="text-center mt-3">
+//         <Link to="/about">About Us</Link>|<Link to="/addcontact">Contact Us</Link>|
+//         <Link to="/policy">Privacy & Policy</Link>|
+//         <Link to="/ratehome">Reviews & Ratings</Link>|
+//         <Link to="/addfeed">Provide Feedback To LEISUREHUB</Link>
+//       </p>
+//       <p className="text-center">
+//         0786453567 | A810,Kaduwela | info@leisurehub.gov.lk
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default Footer;
+
+import React from 'react'
+import { Link } from 'react-router-dom'
+
 const Footer = () => {
   return (
-    <div className="footer">
-      <h4 className="text-center">All Right Reserved &copy; LEISUREHUB</h4>
-      <p className="text-center mt-3">
-        <Link to="/about">About Us</Link>|<Link to="/addcontact">Contact Us</Link>|
-        <Link to="/policy">Privacy & Policy</Link>|
-        <Link to="/ratehome">Reviews & Ratings</Link>|
-        <Link to="/addfeed">Provide Feedback To LEISUREHUB</Link>
-      </p>
-      <p className="text-center">
-        0786453567 | A810,Kaduwela | info@leisurehub.gov.lk
-      </p>
+    <div className = "footer">
+        <h4 className='text-center'>All Right Reserved &copy; LEISUREHUB</h4>
+        <p className='text-center mt-3'>
+            <Link to="/about">About Us</Link>|
+            <Link to="/contact">Contact Us</Link>|
+            <Link to="/policy">Privacy & Policy</Link>|
+            <Link to="/reviews">Reviews & Ratings</Link>|
+            <Link to="/feedback">Provide Feedback To LEISUREHUB</Link>
+        </p>
+        <p className='text-center'>0786453567  |  A810,Kaduwela  |  info@leisurehub.gov.lk</p>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
+
